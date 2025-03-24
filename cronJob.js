aa5bdb73d8f0aa39e64b2885d2c92b6ced565c55
Hamza-Akhtar008 <hamzaakhtar008@gmail.com>
2025-03-24 14:16:17 +0500

    UPDATE BACKEND

--- cronJob.js
+++ cronJob.js
@@ -1,7 +1,7 @@
+const cron = require("node-cron");
+const { fetchTrendingNews } = require("./services/googleTrendsService");
 
-const { fetchAndStoreTrends } = require("./services/googleTrendsService");
-
-cron.schedule("0 */6 * * *", async () => {
+cron.schedule("0 */4 * * *", async () => {
   console.log("⏳ Running fetchTrendingNews...");
   await fetchTrendingNews();
   console.log("✅ fetchTrendingNews completed.");
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db"); // Import the MongoDB connection function
-const { fetchTrendingNews } = require("./services/googleTrendsService");
-const cron = require("node-cron");
+
+
 const articleRoutes = require("./routes/articleroutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const trendroutes = require("./routes/trendRoutes");
@@ -16,11 +16,7 @@ connectDB();
 app.use("/api", articleRoutes); // Use article routes
 app.use("/api/categories", categoryRoutes);
 app.use("/api/trending", trendroutes);
-cron.schedule("0 */4 * * *", async () => {
-  console.log("⏳ Running fetchTrendingNews...");
-  await fetchTrendingNews();
-  console.log("✅ fetchTrendingNews completed.");
-});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
